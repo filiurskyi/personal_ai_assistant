b0f7eb85f1a20bf066e0d7ae2912a61b2b263e38
d43v30n <anjey.mail@gmail.com>
2023-11-23 19:15:24 +0100

    .

--- logic/calendar.py
+++ logic/calendar.py
@@ -12,24 +12,19 @@ def generate_ics_file(events_list: List[Type[Base]]):
     default_event_duration = timedelta(hours=1.0)
     # Create an Event
     date_time_format = "%Y-%m-%d %H:%M:%S"
-    # date_time_ics = "%Y-%m-dT%H:M:%S"
+    date_time_ics = "%Y-%m-dT%H:M:%s"
     for event_dict in events_list:
         evn = event_dict.as_dict()
         event = Event()
         date_time_str = f"{evn.get('ev_date')} {evn.get('ev_time')}"
         event.name = evn.get("ev_title")
-        print(event.name)
         event.begin = datetime.strptime(date_time_str, date_time_format)
-        print(event.begin)
-        event.end = event.begin + default_event_duration
-        print(event.end)
+        event.end = (event.begin + default_event_duration)
         event.description = evn.get("ev_text") + "\n\n" + evn.get("ev_tags")
-        print(event.description)
-        # event.location = ""
 
-        # cal.events.add(event)
+        cal.events.add(event)
+        print("-------------cal-------------\n", cal)
+    with open("./temp/calendar.ics", "w", encoding="utf-8") as f:
+        f.writelines(str(cal))
 
-    # Save the calendar to an .ics file
-    with open("./temp/calendar.ics", "w") as f:
-        f.writelines(cal)
     return "./temp/calendar.ics"
