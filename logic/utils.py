0f358010a909152f03eed267e988d2175ac440c7
d43v30n <anjey.mail@gmail.com>
2023-12-19 19:46:51 +0100

    multilang support

--- logic/utils.py
+++ logic/utils.py
@@ -2,14 +2,17 @@ import re
 from PIL import Image
 import pytesseract
 
+LANGS = ["rus", "ukr", "eng"]
+
 
 def filter_text(text):
-    filtered_text = re.sub(r"[^a-zA-Z0-9\s]", "", text)
+    filtered_text = re.sub(r"[^a-zA-Z0-9а-яА-Я.,:;()!?*+=\-\s]", "", text)
     filtered_text = re.sub(r"\s+", " ", filtered_text)
     return filtered_text
 
 
 def ocr_image(image: Image):
-    custom_config = r'-l eng --psm 3'
-    text = pytesseract.image_to_string(image, config=custom_config)
-    return filter_text(text)
+    text = pytesseract.image_to_string(image, lang="rus+eng+ukr")
+    print(text)
+    filtered_text = filter_text(text)
+    return filtered_text
