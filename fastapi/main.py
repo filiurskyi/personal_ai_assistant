45cd55fed172947e89e0623e7425cd08d6643242
bubliq portable <anjey.mail@gmail.com>
2024-01-04 15:43:30 +0100

    debug

--- fastapi/main.py
+++ fastapi/main.py
@@ -10,11 +10,15 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Initialize Jinja2 templates
 templates = Jinja2Templates(directory="templates")
-book = []
+book = [{'name': '1', 'feedback': 'r3  '}, {'name': '2', 'feedback': 'dsa fsad fs'}]
+
+
 
 # Define the main route for the landing page
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
+    print(book)
+
     return templates.TemplateResponse("index.html", {"request": request, "book": book})
 
 
@@ -24,8 +28,21 @@ async def submit_feedback(request: Request):
     form_data = await request.form()
     name = form_data.get("name")
     feedback = form_data.get("feedback")
-    data = {"name": name, "feedback": feedback}
+    data = {"name": name.strip(), "feedback": feedback.strip()}
     book.append(data)
     # Process feedback (you can save it to a database, etc.)
     # For simplicity, we just return a JSON response with the submitted data.
     return templates.TemplateResponse("form.html", {"request": request, "book": book})
+
+
+@app.post("/delete/{item_name}", response_class=JSONResponse)
+async def delete_item(request: Request, item_name: str):
+    # Find and remove the item with the specified id from the book list
+    for i, item in enumerate(book):
+        print(item)
+        print(item_name)
+        if item.get('name') == item_name:
+            del book[i]
+            break
+    return templates.TemplateResponse("form.html", {"request": request, "book": book})
+    
