335c3ea00ccc2c4ff53cbf422dceb9e83792bdbf
bubliq portable <anjey.mail@gmail.com>
2024-01-05 12:24:32 +0100

    dbg

--- main.py
+++ main.py
@@ -47,4 +47,7 @@ async def main() -> None:
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt as e:
+        print("KeyboardInterrupt: ", e)
